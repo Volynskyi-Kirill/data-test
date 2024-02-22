6adf9b13f8f23bd1a4aa3cae1f00142e99526e71
Kirill108 <kirill108bol@gmail.com>
2024-02-22 15:27:41 +0100

    f

--- nest-app/src/shared/config/cors.config.ts
+++
@@ -1,5 +0,0 @@
-export const CorsConfig = {
-  origin: '*',
-  methods: 'GET,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
--- nest-app/src/shared/constants.ts
+++
@@ -1,1 +0,0 @@
-export const requestsLimit = 50;
